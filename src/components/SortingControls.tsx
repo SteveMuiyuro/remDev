67b6fa9c5352747a379bc84ae43a3005a8ea10a1
SteveMuiyuro <stevemuiyuro@gmail.com>
2024-02-21 15:12:54 +0300

    restructure the Sorting button to account for children

--- src/components/SortingControls.tsx
+++ src/components/SortingControls.tsx
@@ -11,21 +11,22 @@ export default function SortingControls({onClick, sortBy}:SortingControlsProps) 
     <section className="sorting">
       <i className="fa-solid fa-arrow-down-short-wide"></i>
 
-    <SortingButton onClick={()=> onClick("relevant")} isActive ={sortBy === "relevant"}/>
-    <SortingButton onClick={()=> onClick("recent")} isActive ={sortBy === "recent"}/>
+    <SortingButton onClick={()=> onClick("relevant")} isActive ={sortBy === "relevant"}>Relevent</SortingButton>
+    <SortingButton onClick={()=> onClick("recent")} isActive ={sortBy === "recent"}>Recent</SortingButton>
     </section>
   );
 }
 
 type SortingButtonProps ={
   onClick: ()=> void;
-  isActive:boolean
+  isActive:boolean,
+  children:React.ReactNode
 }
 
-function SortingButton({onClick,isActive}:SortingButtonProps){
+function SortingButton({onClick,isActive, children}:SortingButtonProps){
   return(
     <button onClick={onClick}className={`sorting__button sorting__button--recent ${isActive && "sorting__button--active"}`}>
-    Recent
+    {children}
   </button>
   )
   }
